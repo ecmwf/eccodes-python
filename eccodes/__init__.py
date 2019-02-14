219a7eae18fced0ebc8186004a745b3e292da89b
Alessandro Amici <a.amici@bopen.eu>
2019-02-14 11:56:27 +0100

    Copy eccodes legacy code here. Ready for refactoring.

--- eccodes/__init__.py
+++ eccodes/__init__.py
@@ -1,21 +1,11 @@
-#
-# Copyright 2017-2019 European Centre for Medium-Range Weather Forecasts (ECMWF).
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-__version__ = '0.8.0.dev0'
+from __future__ import absolute_import
+import sys
 
 from .eccodes import *
-from .messages import Message, FileStream
+from .eccodes import __version__
+
+if sys.version_info >= (2, 6):
+    from .high_level.gribfile import GribFile
+    from .high_level.gribmessage import GribMessage
+    from .high_level.gribindex import GribIndex
+    from .high_level.bufr import BufrFile, BufrMessage
