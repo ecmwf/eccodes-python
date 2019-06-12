eac8409f0150e345ad3963d4b5abc9f498c7a9a4
Shahram Najm <masn@ecmwf.int>
2019-06-12 16:07:01 +0100

    Function prototypes

--- gribapi/grib_api_prototypes.h
+++ gribapi/grib_api_prototypes.h
@@ -1,7 +1,4 @@
-
 /* grib_handle.c */
 grib_handle *grib_new_from_file(grib_context *c, FILE *f, int headers_only, int *error);
 grib_handle *gts_new_from_file(grib_context *c, FILE *f, int *error);
 grib_handle *metar_new_from_file(grib_context *c, FILE *f, int *error);
-
-int parse_keyval_string(const char *grib_tool, char *arg, int values_required, int default_type, grib_values values[], int *count);
