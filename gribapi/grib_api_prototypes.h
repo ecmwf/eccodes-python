b1ef693d54fe264dfd1796c5b8c68d156a5ea41f
Alessandro Amici <a.amici@bopen.eu>
2019-07-08 18:38:35 +0200

    Remove the unused grib_api_protitype.h

--- gribapi/grib_api_prototypes.h
+++
@@ -1,4 +0,0 @@
-/* grib_handle.c */
-grib_handle *grib_new_from_file(grib_context *c, FILE *f, int headers_only, int *error);
-grib_handle *gts_new_from_file(grib_context *c, FILE *f, int *error);
-grib_handle *metar_new_from_file(grib_context *c, FILE *f, int *error);
