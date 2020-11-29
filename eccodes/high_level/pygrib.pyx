94bab6994ff95df83bb32331d86cd60494d19e89
Jeff Whitaker <jswhit@fastmail.fm>
2020-11-29 09:04:19 -0700

    import pygrib, fix missing value check

--- eccodes/high_level/pygrib.pyx
+++ eccodes/high_level/pygrib.pyx
@@ -1361,7 +1361,10 @@ cdef class gribmessage(object):
                datarr[1::2,:] = datsave[1::2,::-1]
            # if there is a missingValue, and some values missing,
            # create a masked array.
-           if self.has_key('missingValue') and self['numberOfMissing']:
+           #if self.has_key('missingValue') and self['numberOfMissing']:
+           # don't trust numberOfMissing
+           if self.has_key('missingValue') and\
+           np.count_nonzero(datarr==self['missingValue']):
                datarr = ma.masked_values(datarr, self['missingValue'])
         return datarr
 
