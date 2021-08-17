1497a50b3d51705a1dddb538b747e3e3ab4607c3
Shahram Najm <masn@ecmwf.int>
2021-08-17 16:33:50 +0100

    Documentation

--- gribapi/bindings.py
+++ gribapi/bindings.py
@@ -11,6 +11,7 @@
 
 # Authors:
 #   Alessandro Amici - B-Open - https://bopen.eu
+#   Shahram Najm - ECMWF - https://www.ecmwf.int
 #
 
 from __future__ import absolute_import, division, print_function, unicode_literals
