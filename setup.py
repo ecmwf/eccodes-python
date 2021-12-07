afc183b4503b4a0fc35438d74dcd98741fe2984e
Dusan Vuckovic <dusan@dvuckovic.com>
2021-12-07 13:17:12 +0000

    Pinned version of numpy to 1.20 for Python 3.7 and below.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ def parse_version_from(path):
 install_requires = ["numpy"]
 if sys.version_info < (3, 7):
     install_requires = ["numpy<1.20"]
+elif sys.version_info < (3, 8):
+    install_requires = ["numpy<1.22"]
 
 install_requires += ["attrs", "cffi", "findlibs"]
 
