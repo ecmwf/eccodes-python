4fddf0d1cc7636015d5eadb4b7cf8a841b9373b9
Eugen Betke <eugen.betke@ecmwf.int>
2023-03-15 14:49:30 +0000

    ECC-1467: Fix isort error

--- eccodes/eccodes.py
+++ eccodes/eccodes.py
@@ -61,10 +61,10 @@ from gribapi import grib_get_array as codes_get_array
 from gribapi import grib_get_data as codes_grib_get_data
 from gribapi import grib_get_double as codes_get_double
 from gribapi import grib_get_double_array as codes_get_double_array
-from gribapi import grib_get_float_array as codes_get_float_array
 from gribapi import grib_get_double_element as codes_get_double_element
 from gribapi import grib_get_double_elements as codes_get_double_elements
 from gribapi import grib_get_elements as codes_get_elements
+from gribapi import grib_get_float_array as codes_get_float_array
 from gribapi import grib_get_long as codes_get_long
 from gribapi import grib_get_long_array as codes_get_long_array
 from gribapi import grib_get_message as codes_get_message
