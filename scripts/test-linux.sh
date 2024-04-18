fc6c948764a3564408c943ee4e678049bf90b75f
Iain Russell <Iain.Russell@ecmwf.int>
2024-04-18 15:07:16 +0100

    ECC-1793: develop binary wheel - Linux

---
+++ scripts/test-linux.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# (C) Copyright 2024 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+set -eaux
+python_version=$1
+
+ls -l
+source ./scripts/select-python-linux.sh ${python_version}
+
+echo $PATH
+pwd
+ls -l
+
+pip install *.whl
+pip install pytest
+pip install -r tests/requirements.txt
+pip freeze
+
+cd tests
+pytest -v -s
+
+rm -fr *.whl tests
