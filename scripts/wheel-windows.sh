67978c8961f06e8605b8f4cd8c3ae6c05132a795
Iain Russell <Iain.Russell@ecmwf.int>
2024-11-18 09:50:15 +0000

    ECC-1972: support for Windows binary wheel

---
+++ scripts/wheel-windows.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# (C) Copyright 2024- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+set -eaux
+
+
+pip install wheel
+
+rm -fr dist wheelhouse ecmwflibs.egg-info build
+python setup.py bdist_wheel
+mv dist wheelhouse
