d0ff62ccc30dac70daa3459999826650ee9734f9
shahramn <shahram.najm@ecmwf.int>
2025-04-13 11:08:12 +0100

    Bump up version

--- scripts/common.sh
+++ scripts/common.sh
@@ -14,7 +14,7 @@ GIT_ECBUILD=https://github.com/ecmwf/ecbuild.git
 ECBUILD_VERSION=master
 
 GIT_ECCODES=https://github.com/ecmwf/eccodes.git
-ECCODES_VERSION=2.41.0
+ECCODES_VERSION=2.42.0
 ECCODES_COMMON_CMAKE_OPTIONS="-DENABLE_PNG=1 -DENABLE_JPG=1 -DENABLE_NETCDF=0 -DENABLE_EXAMPLES=0"
 
 GIT_AEC=https://github.com/MathisRosenhauer/libaec.git
