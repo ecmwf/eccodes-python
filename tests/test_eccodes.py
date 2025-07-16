baabdc979749fc139d82e4626b441b988928a24a
shahramn <shahram.najm@ecmwf.int>
2025-07-16 16:50:29 +0100

    ECC-2110: GRIB: Set data values for all sample files with parameter temperature to 273.15K

--- tests/test_eccodes.py
+++ tests/test_eccodes.py
@@ -551,7 +551,7 @@ def test_grib_geoiterator():
         [lat, lon, value] = result
         assert -90.0 < lat < 90.00
         assert 0.0 <= lon < 360.0
-        assert math.isclose(value, 1.0, abs_tol=0.001)
+        assert math.isclose(value, 273.15, abs_tol=0.001)
         i += 1
     assert i == 348528
     eccodes.codes_grib_iterator_delete(iterid)
