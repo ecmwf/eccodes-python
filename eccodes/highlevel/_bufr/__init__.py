6b172c6160ca3b46926a4ff449ed349638196426
shahramn <shahram.najm@ecmwf.int>
2025-09-23 14:24:24 +0100

    ECC-2102: black

--- eccodes/highlevel/_bufr/__init__.py
+++ eccodes/highlevel/_bufr/__init__.py
@@ -1,6 +1,7 @@
 """
 A High-level interface for en/decoding of BUFR files.
 """
+
 from .common import change_behaviour, get_behaviour, set_behaviour
 from .helpers import missing_of
 from .message import BUFRMessage
