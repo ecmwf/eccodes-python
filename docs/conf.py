6686741d4937ee219c32aa2c286f522aac3e51a8
shahramn <shahram.najm@ecmwf.int>
2025-12-15 17:01:33 +0000

    GitHub issue: 'python3 setup.py build_sphinx' gives an error #36

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ copyright = "2017-, European Centre for Medium-Range Weather Forecasts (ECMWF)."
 # the built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = pkg_resources.get_distribution("eccodes-python").version
+release = pkg_resources.get_distribution("eccodes").version
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
